f1cd581a0174021e9e6c6d45af1bf26426491301
dsc10694 <136835709+dsc10694@users.noreply.github.com>
2024-02-27 18:09:32 -0500

    number functionality

--- calculator.js
+++ calculator.js
@@ -38,7 +38,7 @@ return multiply(first,second);
 }
 } 
 
-display = document.getElementById('display')
+const display = document.querySelector('.display')
 zeroButton = document.getElementById('0');
 oneButton = document.getElementById('1')
 twoButton = document.getElementById('2')
@@ -49,7 +49,49 @@ sixButton = document.getElementById('6')
 sevenButton = document.getElementById('7')
 eightButton = document.getElementById('8')
 nineButton = document.getElementById('9')
+clearButton = document.querySelector('.clear')
+
+clearButton.addEventListener('click', clear =()=>{
+    display.innerHTML = ''
+})
 
 zeroButton.addEventListener('click' , function() {
     display.textContent += '0';
+    
+});
+
+oneButton.addEventListener('click' , function() {
+    display.textContent += '1';
+});
+
+twoButton.addEventListener('click' , function() {
+    display.textContent += '2';
+});
+
+threeButton.addEventListener('click' , function() {
+    display.textContent += '3';
+});
+
+fourButton.addEventListener('click' , function() {
+    display.textContent += '4';
+});
+
+fiveButton.addEventListener('click' , function() {
+    display.textContent += '5';
+});
+
+sixButton.addEventListener('click' , function() {
+    display.textContent += '6';
+});
+
+sevenButton.addEventListener('click' , function() {
+    display.textContent += '7';
+});
+
+eightButton.addEventListener('click' , function() {
+    display.textContent += '8';
+});
+
+nineButton.addEventListener('click' , function() {
+    display.textContent += '9';
 });
